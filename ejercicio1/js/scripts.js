a462bb7ce1c653e662b428bf32b7a46cfeba35a9
UrielBerardo <berardouriel@gmail.com>
2018-06-01 19:36:40 -0300

    ejercicio1t

--- ejercicio1/js/scripts.js
+++ ejercicio1/js/scripts.js
@@ -1,1 +1,29 @@
-/* here goes the scripts */
+var elementsBuild = {
+   
+    items: [], /*array en donde se tiene muchos elementos, engloba elementos que tienen relacion
+    entre si*/
+
+    listAll: function(){
+        for (var i=0; i < elementsBuild.items.length; i++){
+            $('#table-body').append(elementsBuild.assamble(elementsBuild.items[i])
+            );
+        }/*busca todos los elementos*/
+    },
+
+    assamble: function(item){
+        return '<div class="row">'
+        + '<span class="col-2"></span>'
+        + '<span class="col-2">'+ item.brand +'</span>'
+        + '<span class="col-2">'+ item.location +'</span>'
+        + '<span class="col-2">'+ item.lunch +'</span>'
+        + '<span class="col-2">'+ item.price +'</span>'
+        + '<span class="col-2"></span>'
+        + '<span class="description col-12">'+'</span>'
+      + '</div>';
+    }, /*arma la */
+
+    setItems: function (items){
+        elementsBuild.items = items;
+    },
+ 
+}
